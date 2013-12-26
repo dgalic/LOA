d3063a34da4eb6470926dfba1c5aa650a7e78472
Lasconik <caillouxnicolas@gmail.com>
2013-12-26 21:23:26 +0100

    makefile portable (mais normalement, dépendances pétées)
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

---
+++ src/include/Helpers.hpp
@@ -0,0 +1,9 @@
+#ifndef HELPERS_HPP
+#define HELPERS_HPP
+
+
+unsigned int max(const unsigned int& a, const unsigned int& b);
+
+unsigned int min(const unsigned int& a, const unsigned int& b);
+
+#endif
