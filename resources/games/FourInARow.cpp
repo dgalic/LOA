9c8a8b5e60cee03677fbd02a7f72a241d9a4afcc
Lasconik <caillouxnicolas@gmail.com>
2013-12-26 21:28:04 +0100

    déplacement des dossiers de ressource dans un dossier général
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

---
+++ resources/games/FourInARow.cpp
@@ -0,0 +1,22 @@
+#include "FourInARow.hpp"
+#include "GameState.hpp"
+
+void FourInARow::compute(){
+
+}
+
+void FourInARow::render(){
+
+}
+
+bool FourInARow::init(){
+
+}
+
+bool FourInARow::exit(){
+
+}
+
+StateID::ID FourInARow::getID const{
+  return this->id;
+};
---
+++ resources/games/FourInARow.hpp
@@ -0,0 +1,24 @@
+#ifndef FOURINAROW_HPP
+#define FOURINAROW_HPP
+
+#include "GameState.hpp"
+#include "GUI.hpp"
+
+
+class FourInARow : public GameState{
+
+private:
+  static const StateID::ID id = StateID::FOURINAROW;
+  Matrix<Pion> board;
+
+public:
+  virtual void compute();
+  virtual void render();
+  virtual bool init();
+  virtual bool exit();
+  virtual StateID::ID getID() const { return this->id; }
+  
+
+};
+
+#endif
