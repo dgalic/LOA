9b40cb35b0aa32af275cf0ba0a500ad02e9b03d6
Lasconik <caillouxnicolas@gmail.com>
2013-12-26 21:23:26 +0100

    makefile portable (mais normalement, dépendances pétées)
    Signed-off-by: Nicolas Cailloux <cailloux@informatique.univ-paris-diderot.fr>

---
+++ src/Font.cpp
@@ -0,0 +1,7 @@
+#include "Font.hpp"
+#include "ResourceManager.hpp"
+/*
+Font * FontManager::getLabelFont(){
+  return getInstance()->get("fonts/ArialPixel.ttf");
+}
+*/
---
+++ src/include/Event.hpp
@@ -0,0 +1,10 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#include <SFML/Window/Event.hpp>
+
+class Event:public sf::Event{
+
+};
+
+#endif
