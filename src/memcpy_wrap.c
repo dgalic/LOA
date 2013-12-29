447fd0cddb4c688d1585e693f92771aeaea4909f
Lasconik <caillouxnicolas@gmail.com>
2013-12-29 18:00:55 +0100

    changements pour tenter de rendre portable : compilation OK, pas execution.
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

---
+++ src/memcpy_wrap.c
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#include <string.h>
+
+asm (".symver wrap_memcpy, memcpy@GLIBC_2.14");
+
+void *wrap_memcpy(void *dest, const void * src, size_t n){
+  return memcpy(dest, src, n);
+}
+
