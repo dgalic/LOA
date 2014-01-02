cbf2e50f4bca99e740a0b612b934d01c21353a93
Lasconik <caillouxnicolas@gmail.com>
2014-01-02 22:22:08 +0100

    othello jouable : problème score + bug graphique
    Signed-off-by: Nicolas Cailloux <cailloux@informatique.univ-paris-diderot.fr>

--- src/include/BoardGame.hpp
+++ src/include/BoardGame.hpp
@@ -4,11 +4,21 @@
 #include "Board.hpp"
 #include "State.hpp"
 
+
+#include <set>
+#include <utility> // pour std::pair
+
+/* le type de retour de la fonction de succession : 
+   des paires (x,y) de positions possibles depuis la configuration courante */
+typedef std::set< std::pair< unsigned short, unsigned short> > succ;
+
 class BoardGame : public State{
 
 protected:
   Board board;
   unsigned short pointerX, pointerY; // case séléctionnée
+  bool ingame; //dit si le jeu est terminé ou pas
+  succ successors; //les coups possibles depuis le plateau courant
 
 public:
   BoardGame(const unsigned int&, const unsigned int&);
