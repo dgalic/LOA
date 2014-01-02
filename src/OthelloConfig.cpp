0f2b4f08410182530fdfce887b1b5bd70a7edcbd
Lasconik <caillouxnicolas@gmail.com>
2014-01-03 00:57:32 +0100

    choix de la config pour Othello
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- src/OthelloConfig.cpp
+++ src/OthelloConfig.cpp
@@ -7,6 +7,13 @@
 #include "StateHandler.hpp"
 #include "MainMenuState.hpp"
 
+#include <list>
+#include <sstream>
+
+OthelloConfig::OthelloConfig()
+  :entry(0), type2(0){
+}
+
 OthelloConfig::~OthelloConfig(){
 
 }
@@ -14,24 +21,73 @@ OthelloConfig::~OthelloConfig(){
 void OthelloConfig::handle(const char& c){
   ANSI::Arrow arr = checkArrow(c);
   if(c == 'z' || arr == ANSI::UP){
+    entry = (entry == 0)? entry = 2:entry-1;
     return;
   }
   if(c == 'q' || arr == ANSI::LEFT){
+    if(entry == 0){
+      if( color1 == possible_colors.begin() ){
+	color1 = (possible_colors.end() );
+      }
+      color1--;
+      if(*color1 == *color2){
+	color1--;
+      }
+      return;
+    }
+    if(entry == 1){
+      if( color2 == possible_colors.begin() ){
+	color2 = (possible_colors.end() );
+      }
+      color2--;      
+      if(*color2 == *color1){
+	color2--;
+      }
+      return;
+    }
+    if(entry == 2){
+      type2 = (type2 == 0)? 5:type2-1;
+    }
     return;
   }
   if(c == 's' || arr == ANSI::DOWN){
+    entry = (entry >= 2)? 0 : entry+1;
     return;
   }
   if(c == 'd' || arr == ANSI::RIGHT){
+    if(entry == 0){
+      color1++;
+      if(*color2 == *color1){
+	color1++;
+      }
+      if( color1 == possible_colors.end() ){
+	color1 = (possible_colors.begin() )++;
+      }
+      return;
+    }
+    if(entry == 1){
+      color2++;
+      if(*color2 == *color1){
+	color2++;
+      }
+      if( color2 == possible_colors.end() ){
+	color2 = (possible_colors.begin() )++;
+      }
+      return;
+    }
+    if(entry == 2){
+      type2 = (type2 == 5)? 0:type2+1;
+    }
     return;
   }
 
   if(c == 'p' || c == MARK){
-    switch(entry){
-    case 2:
-      StateHandler::getInstance()->change(new Othello() );
-    default:
-      break;
+    if(color1 != color2){
+      StateHandler::getInstance()->change(new Othello(*color1, *color2, type2) );
+    }else{
+      Console::getInstance()->setForeground(ANSI::WHITE);
+      Console::getInstance()->draw(1, 20, "Les deux joueurs ne peuvent pas avoir la même couleur ! ");
+      Console::getInstance()->setCursor(Console::getInstance()->getWidth(), 0);
     }
     return;
   }
@@ -52,15 +108,42 @@ void OthelloConfig::update(){
 void OthelloConfig::render(){
   Console::getInstance()->clear();
   Console::getInstance()->setForeground(ANSI::Color::WHITE);
-   Console::getInstance()->setCursor(1, 1);
+  Console::getInstance()->setCursor(1, 1);
   Console::getInstance()->draw("Othello  -  z:up  s:down  !/p:select  x:quit");
-  Console::getInstance()->setForeground(ANSI::Color::BLUE);
+  Console::getInstance()->setForeground(ANSI::Color::GRAY);
   Console::getInstance()->drawRectangle(1, 2, Console::getInstance()->getWidth(), 1, '#');
+  Console::getInstance()->setForeground(*color1);
+  Console::getInstance()->draw(4, 4, "Couleur joueur 1");
+  Console::getInstance()->setForeground(*color2);
+  Console::getInstance()->draw(4, 5, "Couleur joueur 2");
+  Console::getInstance()->setForeground(ANSI::Color::WHITE);
+  Console::getInstance()->draw(4, 6, "Type adversaire : "); 
+  if(type2 == 0){
+    Console::getInstance()->draw(30, 6, "Humain");
+  }else{
+    std::ostringstream oss(std::ostringstream::ate);
+    oss.str("IA niveau ");
+    oss << type2;
+    Console::getInstance()->draw(30, 6, oss.str() );
+    oss.clear();
+  }
+  Console::getInstance()->draw(2, 4+entry, '~');
+  
   Console::getInstance()->setCursor(Console::getInstance()->getWidth(), 0);
 }
 
 bool OthelloConfig::init(){
-  entry = 2;
+  possible_colors.push_front(ANSI::BROWN);
+  possible_colors.push_front(ANSI::YELLOW);
+  possible_colors.push_front(ANSI::GREEN);
+  possible_colors.push_front(ANSI::LIGHTCYAN);
+  possible_colors.push_front(ANSI::BLUE);
+  possible_colors.push_front(ANSI::PURPLE);
+  possible_colors.push_front(ANSI::PINK);
+  possible_colors.push_front(ANSI::RED);
+  color1 = (possible_colors.begin() )++;
+  color2 = color1;
+  color2++;
   return true;
 }
 
--- src/include/OthelloConfig.hpp
+++ src/include/OthelloConfig.hpp
@@ -6,14 +6,26 @@
 #include "State.hpp"
 #include "StateHandler.hpp"
 
+#include <list>
+
+/**
+   Config d'othello : 
+   - couleur du joueur 1
+   - couleur du joueur 2
+   - type du joueur 2 : humain ou IA
+ */
+
 class OthelloConfig : public State{
 
 private:
   unsigned short entry;
-  ANSI::Color color1;
-  ANSI::Color color2;
+  unsigned short type2; // 0 pour humain, entre 1 et ?? pour IA
+  std::list<ANSI::Color>::iterator color1;
+  std::list<ANSI::Color>::iterator color2;
+  std::list<ANSI::Color> possible_colors; // couleurs séléctionnables
 
 public:
+  OthelloConfig();
   virtual ~OthelloConfig();
   virtual void handle(const char& = ' ');
   virtual void update();
