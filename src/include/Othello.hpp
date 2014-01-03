e7d359b23517312386417ec7a7f68a9a200e9f2e
Lasconik <caillouxnicolas@gmail.com>
2014-01-03 13:41:15 +0100

    abstraction de la succession : problème de player switch
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- src/include/Othello.hpp
+++ src/include/Othello.hpp
@@ -11,24 +11,21 @@
 class Othello : public BoardGame{
 
 private:
-  ANSI::Color player1;
-  ANSI::Color player2;
-  ANSI::Color currentPlayer;
+  Player player2;
   unsigned short typeIA;
   unsigned short score[2];
 
 public:
-  Othello(ANSI::Color = ANSI::Color::BLUE, 
-	  ANSI::Color = ANSI::Color::GREEN,
+  Othello(Player = ANSI::Color::BLUE, 
+	  Player = ANSI::Color::GREEN,
 	  const unsigned short& = 0);
   virtual ~Othello();
   virtual void handle(const char& = ' ');
-  /** calcule les coups possibles de la configuration (plateau) donné
-   pour le joueur donné */
-  virtual succ next(Board, const ANSI::Color&) const;
-  /** dit si le coup est possible */
-  virtual bool isNext(const unsigned short&, const unsigned short&, const succ&) const;
   virtual void shuffle(const unsigned short&, const unsigned short&);
+  bool isSucc(Board,
+	      const unsigned short&,
+	      const unsigned short&,
+	      const Player&) const;
   virtual void update();
   virtual void render();
   virtual bool init();
