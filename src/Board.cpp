8f3f8aafee899b1689adf77691fbdf4e7b41715a
Lasconik <caillouxnicolas@gmail.com>
2014-01-04 20:45:50 +0100

    ajout de quelques documentations
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,50 +1,64 @@
 #include "Board.hpp"
-#include "Matrix.hpp"
 
 #include "Console.hpp"
+#include "Matrix.hpp"
 
 #include <iostream>
 
+
 Board::Board(const unsigned int& w, const unsigned int& h)
-  :Matrix<int>(w, h){
+  :Matrix<int>(w, h)
+{
+  /**
+   * Constructeur. Crée une matrice de taille @a w x @a h remplie avec @e -1,
+   * qui est la valeur correspondant à "pas de pion dans cette case". 
+   * @param w largeur de la matrice
+   * @param h hauteur de la matrice
+   */
   fillWith(-1); // -1 est la valeur "pas de pion"
 }
 
-Board::~Board(){
+Board::~Board()
+{
 
 }
 
-/*
-  par défaut, car nos jeux de plateau sont dessinés comme ça, 
-  le plateau est dessiné est position (6, 6).
-  Un board est une matrice d'entiers : 
-  -1 correspond à "aucun pion posé".
-  sinon, c'est la couleur de la case.
-  Ainsi, les joueurs devraient être identifiés par leur numéro
-  de couleur, et le Board rempli avec ces identifiants (et non
-  pas 1 pour J1 et 2 pour J2 comme on pourrait s'y attendre).
-*/
+
 void Board::draw(const unsigned int& x = 12,
-		 const unsigned int& y = 8){
+		 const unsigned int& y = 8)
+{
+  /**
+   * Dessine le plateau dans la console à la position (@a x, @a y). Des '|' sont
+   * ajoutés entre chaque colonne, et elle est entre deux lignes de '~'.
+   * La couleur de dessin du @e cadre est blanc. Les pions sont dessinés avec
+   * le caractère @b ʘ, de couleur correspondant au contenu de la matrice 
+   * converti en @e ANSI::Color.
+   * @param x abscisse du point supérieur gauche
+   * @param y abscisse du point supérieur droit
+   */
   Console::getInstance()->setForeground( ANSI::Color::WHITE);
   Console::getInstance()->setCursor(x,y);
-  Console::getInstance()->drawRectangle(x, y, 2*width+1, 1, '~');
-  Console::getInstance()->drawRectangle(x, y+height+1, 2*width+1, 1, '~');
-  
-  for(unsigned int row = 0; row < height; row++){
+  Console::getInstance()->drawRectangle(x, y, 2*getWidth()+1, 1, '~');
+  Console::getInstance()->drawRectangle(x, y+getHeight()+1, \
+                                        2*getWidth()+1, 1, '~');
+  for(unsigned int row = 0; row < getHeight(); row++)
+    {
     unsigned int col = 0;
-    for(col = 0; col < 2*width; col += 2){
+    for(col = 0; col < 2*getWidth(); col += 2)
+      {
       Console::getInstance()->setForeground( ANSI::Color::WHITE);
       Console::getInstance()->draw(x+col, y+row+1, '|');
       int content = at(col/2, row);
-      //      std::cerr<<"Board ("<<row<<","<<col/2<<") -> "<< content <<std::endl;
-      if(content != -1){
+      // std::cerr<<"Board ("<<row<<","<<col/2<<") -> "<< content <<std::endl;
+      if(content != -1)
+        {
 	Console::getInstance()->setForeground( (ANSI::Color)content);
 	Console::getInstance()->draw(x+col+1, y+row+1, "ʘ");
-      }else{
-	Console::getInstance()->draw(x+col+1, y+row+1, ' ');
-      }
-    }  
+        }else
+        {
+          Console::getInstance()->draw(x+col+1, y+row+1, ' ');
+        }
+    }
     Console::getInstance()->setForeground( ANSI::Color::WHITE);
     Console::getInstance()->draw(x+col, y+row+1, '|');
   }
--- src/include/Board.hpp
+++ src/include/Board.hpp
@@ -5,13 +5,25 @@
 
 #include <iostream>
 
-class Board :public Matrix<int>{
+/**
+ * Par défaut, car nos jeux de plateau sont dessinés comme ça,
+ * le plateau est dessiné est position (6, 6).
+ * Un board est une matrice d'entiers :
+ * -1 correspond à "aucun pion posé".
+ * sinon, c'est la couleur de la case.
+ * Ainsi, les joueurs devraient être identifiés par leur numéro
+ * de couleur, et le Board rempli avec ces identifiants (et non
+ * pas 1 pour J1 et 2 pour J2 comme on pourrait s'y attendre).
+*/
+class Board :public Matrix<int>
+{
 
 public:
   Board(const unsigned int&, const unsigned int&);
   //  Board(const Board&);
   virtual ~Board();
-  virtual void draw(const unsigned int&, const unsigned int&);
+  virtual void draw(const unsigned int&, 
+                    const unsigned int&);
 
 };
 
