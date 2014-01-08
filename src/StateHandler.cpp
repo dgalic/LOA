d1093f6df9c6aa43e82e6397e225acdd67881dba
Lasconik <caillouxnicolas@gmail.com>
2014-01-08 22:25:23 +0100

    ajout de doc
    Signed-off-by: Nicolas Cailloux <caillouxnicolas@gmail.com>

--- src/StateHandler.cpp
+++ src/StateHandler.cpp
@@ -4,19 +4,28 @@
 
 #include <iostream>
 
-void StateHandler::push(State * gs){
-  if( gs->init() )
-    states.push(gs);
+void StateHandler::push(State * s){
+  /**
+   * @brief Empile un nouvel état au sommet du StateHandler.
+   * @details L'état posé est le nouvel état courant.
+   * @param s Etat à poser.
+   **/
+  if( s->init() )
+    states.push(s);
   
 }
 
 void StateHandler::pop(){
+  /**
+   * @brief Dépile l'état au sommet du StateHandler.
+   * @details L'état dépilé appelle sa fonction @e exit.
+   **/
   if( states.empty() )
     return;
-  State * gs = states.top();
-  if(gs->exit() ){
+  State * s = states.top();
+  if(s->exit() ){
     states.pop();
-    delete gs;
+    delete s;
   }
 }
 
@@ -24,22 +33,31 @@ bool StateHandler::isEmpty(){
   return states.empty();
 }
 
-void StateHandler::change(State * gs ){
+void StateHandler::change(State * s ){
   pop();
-  push(gs);
+  push(s);
 }
 
 void StateHandler::clear(){
+  /**
+   * @brief Depile tous les états.
+   */
   while( !states.empty() )
     pop();
 }
 
 void StateHandler::update(){
+  /**
+   * @brief Appelle @e update de l'état courant (sommet de la pile).
+   */
   if( not states.empty() )
     states.top()->update();
 }
 
 void StateHandler::render(){
+  /**
+   * @brief Appelle @e render de l'état courant (sommet de la pile).
+   */
   if( not states.empty() )
      states.top()->render();
 }
